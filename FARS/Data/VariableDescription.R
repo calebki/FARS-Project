256939479b84dcc590a099b6a5ae86245dc4a55c
Azka Javaid 17 <ajavaid17@rstudio.amherst.edu>
2016-12-09 10:13:21 -0500

    added ACS variable description

---
+++ FARS/Data/VariableDescription.R
@@ -0,0 +1,10 @@
+
+#B01001_001 to B01001_049: Population by Sex by Age
+#B01003_001: Total Population 
+#B02001_001 to B02001_010: Total by Race
+#C17001_001 to C17001_019: Poverty Status in the Past 12 Months by Sex by Age 
+#C22001_001 to C22001_003: Receipt of Food Stamps/SNAP in the Past 12 Months for Households
+#C27001_001 to C27001_021: Health Insurance Coverage Status by Sex by Age
+#C17002_001 to C17002_008: Ratio of Income to Poverty Level in the Past 12 Months
+#C17003_001 to C17003_011: Poverty Status in the Past 12 Months of Individuals by Education Attainment 
+#C17017_001 to C17017_019: Poverty Status in the Past 12 Months by Household Type 
