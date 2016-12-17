3082321b920b0d07ad4e20af5afbd0546ad09df6
Azka Javaid 17 <ajavaid17@rstudio.amherst.edu>
2016-12-17 11:19:39 -0500

    added changes

--- FARS/Data/ACSRFinal.R
+++ FARS/Data/ACSRFinal.R
@@ -1,3 +1,10 @@
+
+library(acs)
+library(tidyr)
+library(dplyr)
+library(data.table)
+
+
 data("fips.state")
 data("fips.county")
 mygeo <- geo.make(state = "*", county = "*")
@@ -5,3 +12,15 @@ x <- acs.fetch(endyear=2015, geography = mygeo,
                table.number = "B01003", 
                key = "3dbabece4401ad72afa36a118e2cf777efa2afb3")
 countypop <- as.data.frame(estimate(x))
+countypopT <- setDT(countypop, keep.rownames = TRUE)[]
+countypopT <- rename(countypopT, StateCounty = rn)
+countypopT$StateCounty <- as.factor(countypopT$StateCounty)
+levels(countypopT$StateCounty)[levels(countypopT$StateCounty)=="Doña Ana County, New Mexico"] <- "Dona Ana County, New Mexico"
+
+countypopT <- countypopT %>%
+  separate(StateCounty, c("County.Name", "STATE_NAME"), ",")
+
+
+
+#Merge countypopT with fips.state and fips.county 
+
